67e5ca925477b2a56c3d079205e809f9d8688209
rishab <rishab.inder@gmail.com>
2020-05-20 09:46:44 -0700

    testing PR2 for multiplication

--- test.js
+++ test.js
@@ -2,8 +2,14 @@
  * Testing PR
  */
 
-let arr = [1, 2, 3, 4, 5, 6];
-arr = arr.map((val) => {
-  return val % 2 == 0;
-});
-console.log(arr);
+/**
+ * Testing PR#2
+ */
+function multiply(multiplier, ...numbers) {
+  const x = multiplier;
+  return numbers.map((num) => {
+    return x * num;
+  });
+}
+
+console.log(multiply(2, 1, 2, 3, 4, 5, 6));
