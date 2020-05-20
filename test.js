7c4b2b9ce0338d4f9e91480d4994c9a0d18132a7
rishab <rishab.inder@gmail.com>
2020-05-20 09:43:58 -0700

    this is a test

--- test.js
+++ test.js
@@ -7,15 +7,3 @@ arr = arr.map((val) => {
   return val % 2 == 0;
 });
 console.log(arr);
-
-/**
- * Testing PR#2
- */
-function multiply(multiplier, ...numbers) {
-  const x = multiplier;
-  return numbers.map((num) => {
-    return x * num;
-  });
-}
-
-console.log(multiply(2, 1, 2, 3, 4, 5, 6));
