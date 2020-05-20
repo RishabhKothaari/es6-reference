ce5707d3460724915ac84eb54c1967c6da27b83f
rishab <rishab.inder@gmail.com>
2020-05-20 09:41:17 -0700

    this is a test for multiplication

--- test.js
+++ test.js
@@ -6,4 +6,16 @@ let arr = [1, 2, 3, 4, 5, 6];
 arr = arr.map((val) => {
   return val % 2 == 0;
 });
-console.log(arr)
+console.log(arr);
+
+/**
+ * Testing PR#2
+ */
+function multiply(multiplier, ...numbers) {
+  const x = multiplier;
+  return numbers.map((num) => {
+    return x * num;
+  });
+}
+
+console.log(multiply(2, 1, 2, 3, 4, 5, 6));
